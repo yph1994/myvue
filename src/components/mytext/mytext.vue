839eb520f21fcdcde1f10f7632c0550cb3cd3be2
19941101hui= <1162577672@qq.com>
2020-09-16 00:00:46 +0800

    tset2

---
+++ src/components/mytext/mytext.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-button type="success" @click=redirect()>测试</el-button>
+</template>
+
+<script>
+  export default {
+    name:'mytext',
+    data() {
+      return {
+      }
+    },
+    methods: {
+     redirect(){
+         this.$router.replace('/table')
+     }
+    }
+  }
+</script>
---
+++ src/components/mytext/mytext2.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <el-button type="success" @click=redirect()>测试2</el-button>
+  <slot></slot>
+</div>
+</template>
+
+<script>
+  export default {
+    name:'mytext2',
+    data() {
+      return {
+      }
+    },
+    methods: {
+     redirect(){
+         this.$router.push('/table')
+     }
+    }
+  }
+</script>
